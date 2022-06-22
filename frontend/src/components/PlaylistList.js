faa25e38c875c9a4e53e4c91a3d0b5d7e306d9e9
Ethan Oien <oien.ethan.c@gmail.com>
2022-06-22 15:29:05 -0400

    add support for descriptions in the app

--- frontend/src/components/PlaylistList.js
+++ frontend/src/components/PlaylistList.js
@@ -68,7 +68,7 @@ export default function PlaylistList() {
                             <img src={playlist.images[0].url} alt='' />
                             <div className='playlistList-list-item-header-information'>
                                 <span className='playlistList-list-item-header-information-title'>{playlist.name}</span>
-                                <div className='playlistList-list-item-header-information-description'>{/*playlist.description*/}</div>
+                                <div className='playlistList-list-item-header-information-description'>{playlist.description}</div>
                             </div>
                         </div>
                         <CollapseWrapper collapsed={opened !== playlist.id}>
