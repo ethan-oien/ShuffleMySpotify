6698ac5fc0dd51efbf1cf5b01cadfbeb5e09dc4e
Ethan Oien <oien.ethan.c@gmail.com>
2022-06-23 13:36:14 -0400

    remove test.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
 import PlaylistList from './components/PlaylistList';
-import Test from './components/Test';
 
 export default function App() {
 
     return (
         <div className='app'>
-            <Test />
             <h1>Shuffle My Spotify</h1>
             <a href='http://localhost:5000/auth/login'>Login!</a>
             <PlaylistList />
